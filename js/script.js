e30d9fad83e350ea560ce3c9fc5ca716baed0abf
Biswajit-edu <biswajitsubudhi857@gmail.com>
2024-02-10 22:39:22 +0530

    vs-code change

---
+++ js/script.js
@@ -0,0 +1,80 @@
+
+function myFunction() {
+  var x = document.getElementById("my-nav");
+  if (x.className == "text") {
+    x.className += "responsive";
+  } else {
+    x.className = "text";
+  }
+}
+
+function read(){
+  var x = document.getElementById("more");
+  var y = document.getElementById("read");
+  // console.log("hello")
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.innerHTML="Read less"
+  } else {
+    x.style.display = "none";
+    y.innerHTML="Read More";
+  }
+  // x.style.display ="block";
+  ;
+}
+function read1(){
+  var x = document.getElementById("more1");
+  var y = document.getElementById("read1");
+  // console.log("hello")
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.innerHTML="Read less"
+  } else {
+    x.style.display = "none";
+    y.innerHTML="Read More";
+  }
+  // x.style.display ="block";
+  ;
+}
+function read2(){
+  var x = document.getElementById("more2");
+  var y = document.getElementById("read2");
+  // console.log("hello")
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.innerHTML="Read less"
+  } else {
+    x.style.display = "none";
+    y.innerHTML="Read More";
+  }
+  // x.style.display ="block";
+  ;
+}
+function read3(){
+  var x = document.getElementById("more3");
+  var y = document.getElementById("read3");
+  // console.log("hello")
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.innerHTML="Read less"
+  } else {
+    x.style.display = "none";
+    y.innerHTML="Read More";
+  }
+  // x.style.display ="block";
+  ;
+}
+function read4(){
+  var x = document.getElementById("more4");
+  var y = document.getElementById("read4");
+  // console.log("hello")
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.innerHTML="Read less"
+  } else {
+    x.style.display = "none";
+    y.innerHTML="Read More";
+  }
+  // x.style.display ="block";
+  ;
+}
